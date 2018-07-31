3ee5b5170754c7bfa90c95f35dca596a1d13425f
zhangguangyu <zhangguangyu@youbanban.com>
2018-07-31 15:05:58 +0800

    string to array

--- src/android/compress.java
+++ src/android/compress.java
@@ -1,48 +1,34 @@
 package com.youbanban.cordova.compress;
 
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.youbanban.cordova.chooseimages.imageloader.MyAdapter;
-
 import android.app.Activity;
 import android.widget.Toast;
 
-
 public class compress extends CordovaPlugin {
-
 	public static Activity activity;
 	public static CallbackContext callbackContext;
-
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
     throws JSONException {
     	  activity = this.cordova.getActivity();
           this.callbackContext = callbackContext;
         if(action.equals("compress")){
-        	String result = ""; 	
+        	JSONArray result = new JSONArray();
         	for(int i =0;i< args.length();i++){
-				String str1 = args.getString(i);
         		JSONObject jsonObject = new JSONObject(args.getString(i));
         		String width = jsonObject.getString("width");
         		String height = jsonObject.getString("height");
         		String url = jsonObject.getString("url");
-        		result = result + ImageUtils.saveBitmap(activity, url,Integer.parseInt(width),Integer.parseInt(height));
-        		if(i < args.length()-1){
-	            	   result = result + ",";
-        		}
+				result.put(ImageUtils.saveBitmap(activity, url,Integer.parseInt(width),Integer.parseInt(height)));
         	}
         	callbackContext.success(result);
         }
         return true;
     }
-
-
-
 }
 
 
